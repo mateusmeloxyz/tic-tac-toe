d6468d0b8bcab6501e010be3faca511baea5b0c9
Mateus Melo <mateus@mateusmelo.com>
2025-07-31 13:44:20 -0300

    Create Gameboard object to control the board

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Tic tac toe");
+function Gameboard() {
+  const size = 3;
+  const board = Array(size)
+    .fill()
+    .map(() => Array(size).fill(0));
+
+  const getBoard = () => board;
+
+  const markSpot = (posX, posY, playerToken) => {
+    if (board[posX][posY] !== 0) return "Cell already marked";
+
+    if (posX >= size || posX < 0) return "Invalid cell";
+
+    if (posY >= size || posY < 0) return "Invalid cell";
+
+    if (playerToken !== 1 && playerToken !== -1) return "Invalid playerToken";
+
+    board[posX][posY] = playerToken;
+    return `Cell (${posX},${posY}) marked with playerToken ${
+      playerToken === 1 ? "X" : "O"
+    }`;
+  };
+
+  const printBoard = () => {
+    console.table(
+      board.map((row) => {
+        return row.map((spot) => (spot === 1 ? "X" : spot === -1 ? "O" : spot));
+      }),
+    );
+  };
+
+  return { getBoard, markSpot, printBoard };
+}
+
+const board = Gameboard();
+
+board.printBoard();
+console.log(board.markSpot(2, 2, 1));
+board.printBoard();
+console.log(board.markSpot(2, 2, 1));
+board.printBoard();
+console.log(board.markSpot(2, 2, -1));
+board.printBoard();
+console.log(board.markSpot(1, 1, -1));
+board.printBoard();
+console.log(board.markSpot(1, 0, 10));
