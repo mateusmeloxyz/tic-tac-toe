b6edb153fbf3bdcc48915f450273ad2c216f03c1
Mateus Melo <mateus@mateusmelo.com>
2025-08-26 22:32:35 -0300

    Changes cell behavior to resemble conectfour

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
+function Cell() {
+  let value = 0;
+
+  const markSpot = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    markSpot,
+    getValue,
+  };
+}
+
 function Gameboard() {
+  const board = [];
   const size = 3;
-  const board = Array(size)
-    .fill()
-    .map(() => Array(size).fill(0));
+  for (let i = 0; i < size; i++) {
+    board[i] = [];
+    for (let j = 0; j < size; j++) {
+      board[i].push(Cell());
+    }
+  }
 
   const getBoard = () => board;
 
   const markSpot = (posX, posY, playerToken) => {
-    if (board[posX][posY] !== 0) return "Cell already marked";
-
-    if (posX >= size || posX < 0) return "Invalid cell";
-
-    if (posY >= size || posY < 0) return "Invalid cell";
+    if (board[posX][posY].getValue() !== 0) {
+      return "Cell already marked or invalid";
+    }
 
     if (playerToken !== 1 && playerToken !== -1) return "Invalid playerToken";
 
-    board[posX][posY] = playerToken;
+    board[posX][posY].markSpot(playerToken);
     return `Cell (${posX},${posY}) marked with playerToken ${
-      playerToken === 1 ? "X" : "O"
+      playerToken === 1 ? "X" : "o"
     }`;
   };
 
   const printBoard = () => {
     console.table(
       board.map((row) => {
-        return row.map((spot) => (spot === 1 ? "X" : spot === -1 ? "O" : spot));
+        return row.map((spot) =>
+          spot.getValue() === 1
+            ? "X"
+            : spot.getValue() === -1
+              ? "o"
+              : spot.getValue(),
+        );
       }),
     );
   };
@@ -33,6 +56,8 @@ function Gameboard() {
 }
 
 const board = Gameboard();
+
+console.table(board.getBoard());
 
 board.printBoard();
 console.log(board.markSpot(2, 2, 1));
